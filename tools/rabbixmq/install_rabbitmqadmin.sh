ff13fd57d155cb5a057c4ffded719bd27a8a4341
Henrique Pettenuci <riquepet09@gmail.com>
2021-04-08 16:35:37 -0300

    chore:  general file organization

--- tools/rabbixmq/install_rabbitmqadmin.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-cd /tmp
-wget https://raw.githubusercontent.com/rabbitmq/rabbitmq-management/v3.8.9/bin/rabbitmqadmin -o rabbitmqadmin
-rm rabbitmqadmin
-mv rabbitmqadmin.1 rabbitmqadmin
-cp rabbitmqadmin /usr/local/bin/
-chmod +x /usr/local/bin/rabbitmqadmin
